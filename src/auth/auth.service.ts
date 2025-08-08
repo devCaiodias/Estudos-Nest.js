105f5f1c8d54ece441c03510ab9187b981fbdce1
devCaiodias <caiodiasmartins12@gmail.com>
2025-08-08 14:08:44 -0300

    feat: Criando um token JWT no NestJS
    Criando um token JWT no NestJS

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,18 @@
 import { Inject, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
-import { Prisma, User } from 'generated/prisma';
+import { Prisma } from 'generated/prisma';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     @Inject()
     private readonly userService: UserService
 
-    async signin(params: Prisma.UserCreateInput): Promise<Omit<User, 'password'>> {
+    @Inject()
+    private readonly jwtService: JwtService
+
+    async signin(params: Prisma.UserCreateInput): Promise<{access_token: string}> {
         const user = await this.userService.user({email:params.email})
         if (!user) {
              throw new NotFoundException('User not found')
@@ -18,8 +22,8 @@ export class AuthService {
             throw new UnauthorizedException('Invalid credentials')
         }
 
-        const {password, ...result} = user
+        const payload = {sub: user.id}
         
-        return result
+        return {access_token: await this.jwtService.signAsync(payload)}
     }
 }
