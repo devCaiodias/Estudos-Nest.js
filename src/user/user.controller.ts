7c5ec64dfaf0c0d530cd1099aa57d6c418cb0abc
devCaiodias <caiodiasmartins12@gmail.com>
2025-08-09 11:20:59 -0300

    feat: Configuring NestJS AuthGuard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Prisma } from 'generated/prisma';
 import { UserModule } from './user.module';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('user')
 export class UserController {
@@ -14,16 +15,19 @@ export class UserController {
         return this.userService.createUser(userData)
     }
 
+    @UseGuards(AuthGuard)
     @Get(':id')
     async getUser(@Param('id') id: string): Promise<UserModule | null> {
         return this.userService.user({id: Number(id)})
     }
 
+    @UseGuards(AuthGuard)
     @Put(':id')
     async putUser(@Body() userData: Prisma.UserUpdateInput, @Param('id') id: string): Promise<UserModule> {
         return this.userService.updateUser({where: {id: Number(id)}, data: userData})
     }
 
+    @UseGuards(AuthGuard)
     @Delete(':id')
     async deleteUser(@Param('id') id: string): Promise<UserModule> {
         return this.userService.deleteUser({id: Number(id)})
