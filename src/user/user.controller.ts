44f083438dfb4a6431114f88e1600eac3fd06660
devCaiodias <caiodiasmartins12@gmail.com>
2025-08-09 12:05:13 -0300

    feat: Using NestJs CLI to create a complete CRUD

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Prisma } from 'generated/prisma';
 import { UserModule } from './user.module';
@@ -22,7 +22,7 @@ export class UserController {
     }
 
     @UseGuards(AuthGuard)
-    @Put(':id')
+    @Patch(':id')
     async putUser(@Body() userData: Prisma.UserUpdateInput, @Param('id') id: string): Promise<UserModule> {
         return this.userService.updateUser({where: {id: Number(id)}, data: userData})
     }
